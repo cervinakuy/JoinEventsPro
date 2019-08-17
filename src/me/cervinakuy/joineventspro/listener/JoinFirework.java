eec725efcfc6ffe41ec40355903413449f734e5c
Cervinakuy <16023430+cervinakuy@users.noreply.github.com>
2019-08-16 23:15:23 -0700

    Fixed indentation issues

--- src/me/cervinakuy/joineventspro/listener/JoinFirework.java
+++ src/me/cervinakuy/joineventspro/listener/JoinFirework.java
@@ -19,40 +19,42 @@ public class JoinFirework implements Listener {
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		
+		
+		
 		Player p = e.getPlayer();
 		String joinType = (!p.hasPlayedBefore()) ? "FirstJoin" : "Join";
 		
 		if (Config.getBoolean(joinType + ".Other.Firework") && p.hasPermission("jep." + joinType.toLowerCase() + ".firework")) {
 			
-            Firework firework = (Firework) p.getWorld().spawnEntity(p.getLocation(), EntityType.FIREWORK);
-            FireworkMeta fireworkMeta = firework.getFireworkMeta();
-            FireworkEffect.Type type = FireworkEffect.Type.BALL;
-             
-            Random random = new Random();
-            int result = random.nextInt(4) + 1;
-             
-            if (result == 1) {
-            	type = FireworkEffect.Type.BALL;
-            } else if (result == 2) {
-            	type = FireworkEffect.Type.BALL_LARGE;
-            } else if (result == 3) {
-            	type = FireworkEffect.Type.BURST;
-            } else if (result == 4) {
-            	type = FireworkEffect.Type.CREEPER;
-            } else if (result == 5) {
-            	type = FireworkEffect.Type.STAR;
-            }
-             
-            random.nextInt(17);
-            random.nextInt(17);
-             
-            FireworkEffect effect = FireworkEffect.builder().flicker(random.nextBoolean()).withColor(Color.RED).withFade(Color.BLUE).with(type).trail(random.nextBoolean()).build();
-            fireworkMeta.addEffect(effect);
-             
-            int power = random.nextInt(2) + 1;
-             
-            fireworkMeta.setPower(power);
-            firework.setFireworkMeta(fireworkMeta);
+		    Firework firework = (Firework) p.getWorld().spawnEntity(p.getLocation(), EntityType.FIREWORK);
+		    FireworkMeta fireworkMeta = firework.getFireworkMeta();
+		    FireworkEffect.Type type = FireworkEffect.Type.BALL;
+
+		    Random random = new Random();
+		    int result = random.nextInt(4) + 1;
+
+		    if (result == 1) {
+			type = FireworkEffect.Type.BALL;
+		    } else if (result == 2) {
+			type = FireworkEffect.Type.BALL_LARGE;
+		    } else if (result == 3) {
+			type = FireworkEffect.Type.BURST;
+		    } else if (result == 4) {
+			type = FireworkEffect.Type.CREEPER;
+		    } else if (result == 5) {
+			type = FireworkEffect.Type.STAR;
+		    }
+
+		    random.nextInt(17);
+		    random.nextInt(17);
+
+		    FireworkEffect effect = FireworkEffect.builder().flicker(random.nextBoolean()).withColor(Color.RED).withFade(Color.BLUE).with(type).trail(random.nextBoolean()).build();
+		    fireworkMeta.addEffect(effect);
+
+		    int power = random.nextInt(2) + 1;
+
+		    fireworkMeta.setPower(power);
+		    firework.setFireworkMeta(fireworkMeta);
 			
 		}
 		
